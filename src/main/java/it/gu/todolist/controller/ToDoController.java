67126ca422a3f327981cfa1a0262126219d684a4
Guglielmo Tarchi <g.tarchi@dema-engineering.it>
2022-08-11 11:37:10 +0200

    ToDoController - chanched with responseEntity

--- src/main/java/it/gu/todolist/controller/ToDoController.java
+++ src/main/java/it/gu/todolist/controller/ToDoController.java
@@ -10,6 +10,8 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,39 +26,50 @@ import it.gu.todolist.repo.ToDoRepository;
 @RestController
 @RequestMapping("/todo")
 public class ToDoController {
-	
+
 	@Autowired
 	private MongoTemplate template;
-	
+
 	@Autowired
-    private ToDoRepository repo;
-	
+	private ToDoRepository repo;
+
 	@GetMapping("/findAll/{idUser}")
-	public List<ToDo> getUserToDos(@PathVariable String idUser){
-		return repo.findAllByUser(idUser);
+	public ResponseEntity<List<ToDo>> getUserToDos(@PathVariable String idUser){
+		//return repo.findAllByUser(idUser);
+		return new ResponseEntity<>(repo.findAllByUser(idUser), HttpStatus.OK);
 	}
-	
+
 	@PostMapping("/add")
-    public String saveTodo(@RequestBody ToDo td){
-        repo.save(td);
-        return "Added Successfully";
-    }
-	
+	public ResponseEntity<String> saveTodo(@RequestBody ToDo td){
+		try {
+			repo.save(td);
+			return new ResponseEntity<>("Added Succesfully", HttpStatus.OK);
+		}catch(Exception e) {
+			return new ResponseEntity<>(e.toString(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
 	@PostMapping("/update/{id}")
-    public String updateTodo(@PathVariable String id, @RequestBody ToDo td){
-		
-		Document document = new Document();
-		template.getConverter().write(td, document);
-		Update update = new Update();
-		document.forEach(update::set);
-		template.findAndModify(Query.query(Criteria.where("_id").is(id)), update, ToDo.class);
-		
-        return "Updated Successfully";
-    }
-	
+	public ResponseEntity<String> updateTodo(@PathVariable String id, @RequestBody ToDo td){
+		try {
+			Document document = new Document();
+			template.getConverter().write(td, document);
+			Update update = new Update();
+			document.forEach(update::set);
+			template.findAndModify(Query.query(Criteria.where("_id").is(id)), update, ToDo.class);
+			return new ResponseEntity<>("Updated Succesfully", HttpStatus.OK);
+		}catch(Exception e) {
+			return new ResponseEntity<>(e.toString(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
 	@DeleteMapping("/delete/{id}")
-	public String deletTodo(@PathVariable String id) {
-		repo.deleteById(id);
-		return "Deleted Succesfully";
+	public ResponseEntity<String> deletTodo(@PathVariable String id) {
+		try {
+			repo.deleteById(id);
+			return new ResponseEntity<>("Deleted Succesfully", HttpStatus.OK);
+		}catch(Exception e) {
+			return new ResponseEntity<>(e.toString(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 }
